e34636d5033c328fbc6610d4fcfded3ca576b35d
Joshua <jdlawsin@gmail.com>
2026-03-27 00:24:47 +0800

    Add profiles table and migration

---
+++ prisma/migrations/20260326145225_add_profile/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'CLIENT');
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "fullName" TEXT,
+    "avatarUrl" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'CLIENT',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_email_key" ON "profiles"("email");
